8686edf00bb6196eff71f963e765afe8ca21086f
yangjieshao <yangjie123098@126.com>
2016-04-16 12:53:16 +0800

    二维码生成工具

---
+++ QRCode/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace QRCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.encodeButton = new System.Windows.Forms.Button();
+            this.decodeButton = new System.Windows.Forms.Button();
+            this.openPicButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.logoPictureBox = new System.Windows.Forms.PictureBox();
+            this.logoImportButton = new System.Windows.Forms.Button();
+            this.logiClearbutton = new System.Windows.Forms.Button();
+            this.clearPicButton = new System.Windows.Forms.Button();
+            this.savePicButton = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Location = new System.Drawing.Point(12, 233);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(238, 250);
+            this.textBox1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AllowDrop = true;
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(256, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(562, 442);
+            this.flowLayoutPanel1.TabIndex = 1;
+            this.flowLayoutPanel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.flowLayoutPanel1_DragDrop);
+            this.flowLayoutPanel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.flowLayoutPanel1_DragEnter);
+            // 
+            // encodeButton
+            // 
+            this.encodeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.encodeButton.Location = new System.Drawing.Point(574, 462);
+            this.encodeButton.Name = "encodeButton";
+            this.encodeButton.Size = new System.Drawing.Size(75, 23);
+            this.encodeButton.TabIndex = 2;
+            this.encodeButton.Text = "生成";
+            this.encodeButton.UseVisualStyleBackColor = true;
+            this.encodeButton.Click += new System.EventHandler(this.encodeButton_Click);
+            // 
+            // decodeButton
+            // 
+            this.decodeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.decodeButton.Location = new System.Drawing.Point(655, 462);
+            this.decodeButton.Name = "decodeButton";
+            this.decodeButton.Size = new System.Drawing.Size(75, 23);
+            this.decodeButton.TabIndex = 3;
+            this.decodeButton.Text = "解析";
+            this.decodeButton.UseVisualStyleBackColor = true;
+            this.decodeButton.Click += new System.EventHandler(this.decodeButton_Click);
+            // 
+            // openPicButton
+            // 
+            this.openPicButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.openPicButton.Location = new System.Drawing.Point(256, 460);
+            this.openPicButton.Name = "openPicButton";
+            this.openPicButton.Size = new System.Drawing.Size(75, 23);
+            this.openPicButton.TabIndex = 4;
+            this.openPicButton.Text = "打开图片";
+            this.openPicButton.UseVisualStyleBackColor = true;
+            this.openPicButton.Click += new System.EventHandler(this.openPicButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 206);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "内容:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Logo:";
+            // 
+            // logoPictureBox
+            // 
+            this.logoPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.logoPictureBox.Location = new System.Drawing.Point(65, 9);
+            this.logoPictureBox.Name = "logoPictureBox";
+            this.logoPictureBox.Size = new System.Drawing.Size(80, 80);
+            this.logoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.logoPictureBox.TabIndex = 7;
+            this.logoPictureBox.TabStop = false;
+            // 
+            // logoImportButton
+            // 
+            this.logoImportButton.Location = new System.Drawing.Point(164, 12);
+            this.logoImportButton.Name = "logoImportButton";
+            this.logoImportButton.Size = new System.Drawing.Size(75, 23);
+            this.logoImportButton.TabIndex = 8;
+            this.logoImportButton.Text = "导入";
+            this.logoImportButton.UseVisualStyleBackColor = true;
+            this.logoImportButton.Click += new System.EventHandler(this.logoImportButton_Click);
+            // 
+            // logiClearbutton
+            // 
+            this.logiClearbutton.Location = new System.Drawing.Point(164, 41);
+            this.logiClearbutton.Name = "logiClearbutton";
+            this.logiClearbutton.Size = new System.Drawing.Size(75, 23);
+            this.logiClearbutton.TabIndex = 9;
+            this.logiClearbutton.Text = "Clear";
+            this.logiClearbutton.UseVisualStyleBackColor = true;
+            this.logiClearbutton.Click += new System.EventHandler(this.logiClearbutton_Click);
+            // 
+            // clearPicButton
+            // 
+            this.clearPicButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.clearPicButton.Location = new System.Drawing.Point(493, 462);
+            this.clearPicButton.Name = "clearPicButton";
+            this.clearPicButton.Size = new System.Drawing.Size(75, 23);
+            this.clearPicButton.TabIndex = 10;
+            this.clearPicButton.Text = "清空";
+            this.clearPicButton.UseVisualStyleBackColor = true;
+            this.clearPicButton.Click += new System.EventHandler(this.clearPicButton_Click);
+            // 
+            // savePicButton
+            // 
+            this.savePicButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.savePicButton.Location = new System.Drawing.Point(736, 462);
+            this.savePicButton.Name = "savePicButton";
+            this.savePicButton.Size = new System.Drawing.Size(75, 23);
+            this.savePicButton.TabIndex = 11;
+            this.savePicButton.Text = "保存图片";
+            this.savePicButton.UseVisualStyleBackColor = true;
+            this.savePicButton.Click += new System.EventHandler(this.savePicButton_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(105, 95);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(134, 21);
+            this.numericUpDown1.TabIndex = 12;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "方格尺寸";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 12);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "名字中需排除的内容:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(14, 175);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(225, 21);
+            this.textBox2.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "容错率";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "L",
+            "M",
+            "Q",
+            "H"});
+            this.comboBox1.Location = new System.Drawing.Point(105, 126);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 497);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.savePicButton);
+            this.Controls.Add(this.clearPicButton);
+            this.Controls.Add(this.logiClearbutton);
+            this.Controls.Add(this.logoImportButton);
+            this.Controls.Add(this.logoPictureBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.openPicButton);
+            this.Controls.Add(this.decodeButton);
+            this.Controls.Add(this.encodeButton);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button encodeButton;
+        private System.Windows.Forms.Button decodeButton;
+        private System.Windows.Forms.Button openPicButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox logoPictureBox;
+        private System.Windows.Forms.Button logoImportButton;
+        private System.Windows.Forms.Button logiClearbutton;
+        private System.Windows.Forms.Button clearPicButton;
+        private System.Windows.Forms.Button savePicButton;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ QRCode/Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+using System.Windows.Forms;
+
+using ThoughtWorks.QRCode.Codec;
+using ThoughtWorks.QRCode.Codec.Data;
+
+namespace QRCode
+{
+    public partial class Form1 : Form
+    {
+        private readonly char[] _pathInvalidFileNameChars = Path.GetInvalidFileNameChars();
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 2;
+        }
+
+        private void encodeButton_Click(object sender, EventArgs e)
+        {
+            CleanflowLayoutPanelData();
+            StringReader sr = new StringReader(textBox1.Text);
+
+            string str =sr.ReadLine();
+            do
+            {
+                if (!string.IsNullOrEmpty(str))
+                {
+                    Bitmap qRCodeBitmap = GetQrCode(str);
+                    {
+                        using (Bitmap bLogo = GetLogoBitmap(qRCodeBitmap.Size.Width/5))
+                        {
+                            if (bLogo != null)
+                            {
+                                Point point = new Point((qRCodeBitmap.Size.Width / 2) - (bLogo.Size.Width / 2),
+                                                        (qRCodeBitmap.Size.Height / 2) - (bLogo.Size.Height / 2));
+                                using (Graphics g = Graphics.FromImage(qRCodeBitmap))
+                                {
+                                    g.DrawImage(bLogo, point);
+                                }
+                            }
+                        }
+                    }
+
+                    PictureBox qRCodePicBox = GetQrCodePictureBox(qRCodeBitmap);
+                    string nameStr =string.IsNullOrEmpty(textBox2.Text)?str: str.Replace(textBox2.Text, string.Empty).Trim();
+
+                    qRCodePicBox.Name = string.IsNullOrEmpty(nameStr)? Guid.NewGuid().ToString():nameStr;
+                    foreach (char pathInvalidFileNameChar in _pathInvalidFileNameChars)
+                    {
+                        qRCodePicBox.Name = qRCodePicBox.Name.Replace(pathInvalidFileNameChar, '|');
+                    }
+                    qRCodePicBox.Name = qRCodePicBox.Name.Replace(@"|", String.Empty);
+                    flowLayoutPanel1.Controls.Add(qRCodePicBox);
+                }
+                str = sr.ReadLine();
+            }
+            while (str != null);
+        }
+
+        private void decodeButton_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            StringBuilder myStringBuilder = new StringBuilder();    
+            foreach (Control control in flowLayoutPanel1.Controls)
+            {
+                try
+                {
+                    QRCodeDecoder decoder = new QRCodeDecoder();
+                    PictureBox box = control as PictureBox;
+                    if (box != null)
+                        myStringBuilder.AppendLine(decoder.decode(new QRCodeBitmapImage(new Bitmap(box.Image)), Encoding.UTF8));
+                }
+                catch
+                {
+                    // ignored
+                }
+            }
+            textBox1.Text = myStringBuilder.ToString();
+        }
+
+        private void openPicButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog
+            {
+                InitialDirectory = "C://",
+                Filter =
+                    @"图片(*.bmp;*.jpg;*.jpeg;*.jpe;*.png;*.ico)|*.bmp;*.jpg;*.jpeg;*.jpe;*.png;*.ico|All files (*.*)|*.*",
+                FilterIndex = 1,
+                RestoreDirectory = true,
+                Multiselect = true
+            };
+
+
+
+
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string path in fileDialog.FileNames)
+                {
+                    PictureBox qRCodePicBox = GetQrCodePictureBox(path);
+                    flowLayoutPanel1.Controls.Add(qRCodePicBox);
+                }
+            }
+        }
+
+        private void logoImportButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog
+            {
+                InitialDirectory = "C://",
+                Filter =
+                    @"图片(*.bmp;*.jpg;*.jpeg;*.jpe;*.png;*.ico)|*.bmp;*.jpg;*.jpeg;*.jpe;*.png;*.ico|All files (*.*)|*.*",
+                FilterIndex = 1,
+                RestoreDirectory = true
+            };
+
+
+
+
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                logoPictureBox.ImageLocation = fileDialog.FileName;
+            }
+        }
+
+        private void logiClearbutton_Click(object sender, EventArgs e)
+        {
+            logoPictureBox.Image = null;
+        }
+
+        private Bitmap GetQrCode(string code)
+        {
+            QRCodeEncoder qrEntity = new QRCodeEncoder
+            {
+                QRCodeEncodeMode = QRCodeEncoder.ENCODE_MODE.BYTE, // 二维码编码方式
+                QRCodeScale = Convert.ToInt32(numericUpDown1.Value), // 每个小方格的宽度
+                QRCodeVersion = 1, // 二维码版本号
+                QRCodeErrorCorrect = (QRCodeEncoder.ERROR_CORRECTION)Enum.Parse(typeof(QRCodeEncoder.ERROR_CORRECTION), comboBox1.SelectedItem.ToString())//QRCodeEncoder.ERROR_CORRECTION.M // 纠错码等级
+            };
+
+            Bitmap srcimage;
+            //动态调整二维码版本号,上限40，过长返回空白图片，编码后字符最大字节长度2953
+            while (true)
+            {
+                try
+                {
+                    srcimage = qrEntity.Encode(code, Encoding.UTF8);
+                    break;
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    if (qrEntity.QRCodeVersion < 40)
+                    {
+                        qrEntity.QRCodeVersion++;
+                    }
+                    else
+                    {
+                        srcimage = new Bitmap(100, 100);
+                        break;
+                    }
+                }
+            }
+            return srcimage;
+        }
+
+        private Bitmap GetLogoBitmap(int sizeWidth)
+        {
+            Bitmap logoBitmap = null;
+            if (logoPictureBox.Image != null)
+            {
+                logoBitmap = new Bitmap(logoPictureBox.Image, sizeWidth, sizeWidth);
+            }
+            return logoBitmap;
+        }
+
+        private PictureBox GetQrCodePictureBox(Bitmap qRCodeBitmap)
+        {
+            PictureBox pb = CreatePictureBox();
+            pb.Image = qRCodeBitmap;
+            return pb;
+        }
+        private PictureBox GetQrCodePictureBox(string picPath)
+        {
+            PictureBox pb = CreatePictureBox();
+            pb.ImageLocation = picPath;
+            pb.Name = Path.GetFileNameWithoutExtension(picPath);
+            return pb;
+        }
+        private PictureBox CreatePictureBox()
+        {
+            return new PictureBox()
+            {
+                BorderStyle = BorderStyle.FixedSingle,
+                Size = new Size(100, 100),
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                Padding = new Padding(4)
+            };
+        }
+
+        private void clearPicButton_Click(object sender, EventArgs e)
+        {
+            CleanflowLayoutPanelData();
+        }
+
+        private void CleanflowLayoutPanelData()
+        {
+            
+            foreach (Control control in flowLayoutPanel1.Controls)
+            {
+                if (control is PictureBox)
+                {
+                    PictureBox pb = control as PictureBox;
+                    if (pb.Image != null)
+                    {
+                        Image oldImage = pb.Image;
+                        pb.Image = null;
+                        oldImage.Dispose();
+                    }
+                    pb.Dispose();
+                }
+            }
+            flowLayoutPanel1.Controls.Clear();
+            GC.Collect();
+        }
+        private void flowLayoutPanel1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] strs = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            foreach (string path in strs)
+            {
+                PictureBox qRCodePicBox = GetQrCodePictureBox(path);
+                flowLayoutPanel1.Controls.Add(qRCodePicBox);
+            }
+        }
+
+        private void flowLayoutPanel1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void savePicButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fileDialog = new FolderBrowserDialog
+            {
+                Description = @"请选择保存输出图件的文件夹",
+                ShowNewFolderButton = true,
+                RootFolder = Environment.SpecialFolder.MyComputer
+            };
+            
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                foreach (Control control in flowLayoutPanel1.Controls)
+                {
+                    var pictureBox = control as PictureBox;
+                    if (pictureBox != null)
+                        pictureBox.Image.Save(fileDialog.SelectedPath + "\\" + pictureBox.Name + ".png"
+                            , ImageFormat.Png);
+                }
+            }
+        }
+    }
+}
