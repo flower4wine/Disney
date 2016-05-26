5d4ef63af11ef85a76efea058e44b1f98738a452
thomasZhang1982 <93240455@qq.com>
2016-05-26 17:40:19 +0800

    去掉部分硬代码
    去掉部分硬代码

---
+++ server/disney/src/main/java/com/disney/web/controller/generate/GenerateFix.java
@@ -0,0 +1,11 @@
+package com.disney.web.controller.generate;
+
+public class GenerateFix {
+	
+	public static int P1_QRCODE_NUM = 32;
+	public static int P2_QRCODE_NUM = 113;
+	public static int P3_QRCODE_NUM = 63;
+	public static int P4_QRCODE_NUM = 74;
+	
+
+}
