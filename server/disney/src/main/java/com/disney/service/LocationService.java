204dc6bd2f8accb44d4fb1b9bd85d07a66a99dfe
thomasZhang1982 <93240455@qq.com>
2016-05-27 20:22:58 +0800

    清理代码
    清理代码

--- server/disney/src/main/java/com/disney/service/LocationService.java
+++ server/disney/src/main/java/com/disney/service/LocationService.java
@@ -2,33 +2,15 @@ package com.disney.service;
 
 import java.util.List;
 
-import com.disney.model.FromToOptimize;
 import com.disney.model.Location;
 import com.disney.model.QrCode;
 import com.disney.model.UserLocation;
 
 public interface LocationService {
 	
-	/**
-	 * 数据清理 
-	 */
-	public void cleanData();
 	
 	public List<Location> findAll();
 	
-	/**
-	 * 保存
-	 * @param location
-	 * @return
-	 */
-	public void save(Location location);
-	
-	/**
-	 * 保存
-	 * @param location
-	 * @return
-	 */
-	public void update(Location location);
 	
 	/**
 	 * 查询位置
@@ -37,34 +19,6 @@ public interface LocationService {
 	 */
 	public Location find(String qrCode);
 	
-	
-	/**
-	 * 增加二维码
-	 * @param code
-	 * @param name
-	 * @param qrCodeType
-	 */
-	public void addQrCode(String code, String name,Integer qrCodeType);
-	
-
-	public void addFromTo(FromToOptimize fromTo); 
-	
-	/**
-	 * 
-	 * @param from
-	 * @param to
-	 * @return
-	 */
-	@Deprecated
-	public FromToOptimize getFromToStartWith(String from,String to);
-	
-	/**
-	 * 
-	 * @param from
-	 * @param to
-	 * @return
-	 */
-	public FromToOptimize getFromToInfo(String from,String to);
 	
 	/**
 	 * 
@@ -87,9 +41,6 @@ public interface LocationService {
 	public QrCode findQrCode(String qrCodeUrl);
 	
 	
-	void updateQrCode(QrCode code);
-
-	List<QrCode> allQrCodes();
 	
 	
 }
